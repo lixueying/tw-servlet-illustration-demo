b503028009d936103a7b01b10bf530e2534113d9
linesh-simplicity <linesh.simpcity@gmail.com>
2015-12-12 03:38:41 +0800

    Linesh: First commit.

---
+++ src/main/java/com/thoughtworks/webrecap/HelloWorldServlet.java
@@ -0,0 +1,24 @@
+package com.thoughtworks.webrecap;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userName = request.getParameter("sayMyName");
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<h1>" + "Hello " + userName + "!" + "</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Welcome</title>
+  </head>
+  <body>
+    <p>
+      Welcome!
+    </p>
+  </body>
+</html>
